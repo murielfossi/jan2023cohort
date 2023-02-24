d41ef40d3285f49f751889659a150c7f3bea0ac2
murielfossi <mina78aaa@gmail.com>
2023-02-23 19:00:07 -0600

    update

--- lectures/cSharp/week2/day1/GitOctoCats/Controllers/StringController.cs
+++ lectures/cSharp/week2/day1/GitOctoCats/Controllers/StringController.cs
@@ -11,12 +11,12 @@ namespace GitOctoCats.Controllers;
         // public ViewResult Index() {
         //     return View();
         // }
-        [HttpGet("/strings/{name}")]
+        [HttpGet("/strings/{name}")]//ici on testait comment si on change le nom a chaque foi la phrase aura un nouveau nom
         public string HelloName(string name) {
             return $"Hello there {name}, how are you today?";
         }
 
-        [HttpGet("/strings/math/{a}/{b}")]
+        [HttpGet("/strings/math/{a}/{b}")]//ici on rtestait comment ecrire avec les variable, si on tap strings/math/3/7 sur le browser on aura un resulta.
         public string Math(int a, int b) {
             int c = a * b;
             return $"You gave me {a} and {b}.  I have added them to now give you back {c}";
