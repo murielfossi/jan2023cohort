9613783b11392b3f6929ef311aa17e4508a93ac1
murielfossi <mina78aaa@gmail.com>
2023-02-24 16:46:55 -0600

    Update

--- assignments/cSharp/DojoSurvey/Controllers/FormController.cs
+++ assignments/cSharp/DojoSurvey/Controllers/FormController.cs
@@ -2,23 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace DojoSurvey.Controllers;
     public class FormController : Controller// make sure of this line
     {
-        [HttpGet]
-        [Route("/result")]
+        // [HttpGet]
+        // [Route("/result")]
        
-        public ViewResult Result() {
-            return View();
-        }
+        // public ViewResult Result() {
+        //     return View();
+        // }
 
 
         
-        [HttpPost("/result")]
+      [HttpPost("/result")]
         public IActionResult Result( string UserName, string Location, string Language, string Message ){
            ViewBag.UserName = $"{UserName}";
             ViewBag.Location = $"{Location}";
             ViewBag.Language = $"{Language}";
             ViewBag.Message =$"{Message}";
              
-             return View("");
+             return View();
         }
         
 
